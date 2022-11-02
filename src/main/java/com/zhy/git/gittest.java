6ecf24a1dd5f06a6f66100d12a426952f9cc5611
zhy <1725360323@qq.com>
2022-11-02 16:19:57 +0800

    hti-fix commit1

--- src/main/java/com/zhy/git/gittest.java
+++ src/main/java/com/zhy/git/gittest.java
@@ -6,7 +6,7 @@ public class gittest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
-        System.out.println("hello git4!");
+        System.out.println("hello git5!");
 
     }
 }
