8647baa3acb9a9da748485203c357491fe1c126c
zhy <1725360323@qq.com>
2022-11-02 16:08:56 +0800

    second commit

--- src/main/java/com/zhy/git/gittest.java
+++ src/main/java/com/zhy/git/gittest.java
@@ -4,5 +4,6 @@ package com.zhy.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
