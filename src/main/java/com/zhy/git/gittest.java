8f52b9a9531eb7ac7f19bca8445b3258efff2d51
zhy <1725360323@qq.com>
2022-11-07 18:36:45 +0800

    push commit2

--- src/main/java/com/zhy/git/gittest.java
+++ src/main/java/com/zhy/git/gittest.java
@@ -8,6 +8,6 @@ public class gittest {
         System.out.println("hello git3!");
         System.out.println("hello git5!");
         System.out.println("master test");
-        System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
